549a14576d83ccd4f5a03e2c66436e70f8b94db2
keskarshreesh <shreesh.keskar@tsgforce.com>
2023-11-20 11:16:40 -0500

    first commit

---
+++ resnet_core.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        nn.BatchNorm2d(out_channels),
+                        nn.ReLU())
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
+                        nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
+        
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.downsample:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self):
+        ## initialize ResNet50
+        pass
+    
+    
+    def forward(self, x):
+        # ResNet50 forward pass
+        pass
