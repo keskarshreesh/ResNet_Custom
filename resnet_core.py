6785776770ef5be0d3fe9e1d8a37e2e85b3610d3
keskarshreesh <shreesh.keskar@tsgforce.com>
2023-11-29 18:59:27 -0500

    ResNet50 model code complete

--- resnet_core.py
+++ resnet_core.py
@@ -1,15 +1,19 @@
+import torch
 import torch.nn as nn
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride = 1, downsample = None):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Sequential(
-                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 1, stride = stride, padding = 1),
                         nn.BatchNorm2d(out_channels),
                         nn.ReLU())
         self.conv2 = nn.Sequential(
                         nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
                         nn.BatchNorm2d(out_channels))
+        self.conv3 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels*4, kernel_size = 1, stride = 1, padding = 1),
+                        nn.BatchNorm2d(out_channels*4))
         self.downsample = downsample
         self.relu = nn.ReLU()
         self.out_channels = out_channels
@@ -18,18 +22,61 @@ class ResidualBlock(nn.Module):
         residual = x
         out = self.conv1(x)
         out = self.conv2(out)
+        out = self.conv3(out)
         if self.downsample:
             residual = self.downsample(x)
         out += residual
         out = self.relu(out)
         return out
 
-class ResNet(nn.Module):
-    def __init__(self):
+class ResNet50(nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
         ## initialize ResNet50
-        pass
+        super(ResNet50, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(3, 64, kernel_size = 7, stride = 2, padding = 3, bias=False),
+                        nn.BatchNorm2d(64),
+                        nn.ReLU())
+        self.maxpool = nn.MaxPool2d(kernel_size = 3, stride = 2, padding = 1)
+
+        self.conv2 = self._make_layer(block, 64, layers[0])
+        self.conv3 = self._make_layer(block, 128, layers[1])
+        self.conv4 = self._make_layer(block, 256, layers[2])
+        self.conv5 = self._make_layer(block, 512, layers[3])
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(2048, num_classes)
     
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * 4, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * 4),
+            )
+            
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * 4
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
+
+        return nn.Sequential(*layers)
     
     def forward(self, x):
         # ResNet50 forward pass
-        pass
+        x = self.conv1(x)
+        x = self.maxpool(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+def resnet50():
+    return ResNet50(ResidualBlock, [3, 4, 6, 3])
