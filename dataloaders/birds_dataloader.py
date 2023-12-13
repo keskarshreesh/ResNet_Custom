f2e07d097bf8056b9c78ef20761c2073f96b81d4
keskarshreesh <keskarshreesh@gmail.com>
2023-12-13 13:43:14 -0500

    Dataloader changes

--- dataloaders/birds_dataloader.py
+++ dataloaders/birds_dataloader.py
@@ -2,7 +2,7 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
 transform = transforms.Compose([
-    transforms.Resize(224),  # Resize to 224x224
+    transforms.Resize((224,224)),  # Resize to 224x224
     transforms.ToTensor(),   # Convert to tensor
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
 ])
