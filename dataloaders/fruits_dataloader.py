6b3384b9256d82524901b1e3ee4dd50716857d39
keskarshreesh <keskarshreesh@gmail.com>
2023-12-13 13:18:50 -0500

    Training bug fix

--- dataloaders/fruits_dataloader.py
+++ dataloaders/fruits_dataloader.py
@@ -3,20 +3,20 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 
 transform = transforms.Compose([
-    transforms.Resize(224),  # Resize to 224x224
+    transforms.Resize((224,224)),  # Resize to 224x224
     transforms.ToTensor(),   # Convert to tensor
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
 ])
 
-train_dataset = datasets.ImageFolder(root='../../../datasets/fruits/fruits-360_dataset/fruits-360/Training', transform=transform)
-test_dataset_full = datasets.ImageFolder(root='../../../datasets/fruits/fruits-360_dataset/fruits-360/Test', transform=transform)
+train_dataset = datasets.ImageFolder(root='/common/users/skk139/ResNet_Custom/datasets/fruits/fruits-360_dataset/fruits-360/Training', transform=transform)
+test_dataset_full = datasets.ImageFolder(root='/common/users/skk139/ResNet_Custom/datasets/fruits/fruits-360_dataset/fruits-360/Test', transform=transform)
 
 batch_size = 16
 
 train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 
 total_test_size = len(test_dataset_full)
-test_size = len(total_test_size)*0.6
+test_size = int(total_test_size*0.6)
 valid_size = total_test_size - test_size
 
 test_dataset, valid_dataset = random_split(test_dataset_full, [test_size, valid_size])
